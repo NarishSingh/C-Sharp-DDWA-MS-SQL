de91f759d3810a97370378bb2aa66e16f9f3ad65
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-19 16:11:06 -0500

    starting DDL lesson script

---
+++ MS SQL Scripts/RosterDDL.sql
@@ -0,0 +1,27 @@
+﻿USE master;
+GO
+
+CREATE DATABASE SGRoster;
+GO
+
+USE SGRoster;
+GO
+
+CREATE TABLE Cohort
+(
+    CohortId   INT IDENTITY (1,1) PRIMARY KEY,
+    StartDate  DATE        NOT NULL,
+    [Subject]  VARCHAR(30) NOT NULL,
+    [Location] VARCHAR(30) NOT NULL
+)
+GO
+
+CREATE TABLE Apprentice
+(
+    ApprenticeId INT IDENTITY (1,1) PRIMARY KEY,
+    FirstName    VARCHAR(30) NOT NULL,
+    LastName     VARCHAR(30) NOT NULL,
+    CohortId     INT FOREIGN KEY
+        REFERENCES Cohort (CohortId)
+)
+GO
