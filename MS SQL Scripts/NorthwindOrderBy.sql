bdfbe60ac16ebdf74de839ebecadc129d289ba7a
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-23 12:59:34 -0500

    Adding Order By lesson script

---
+++ MS SQL Scripts/NorthwindOrderBy.sql
@@ -0,0 +1,42 @@
+﻿USE Northwind;
+GO
+
+SELECT CategoryName, ProductName, UnitPrice, UnitsInStock
+FROM Categories C
+         JOIN Products P ON C.CategoryID = P.CategoryID
+ORDER BY CategoryName;
+
+-- multiple ordering
+SELECT CategoryName, ProductName, UnitPrice, UnitsInStock
+FROM Categories C
+         JOIN Products P ON C.CategoryID = P.CategoryID
+ORDER BY CategoryName, ProductName;
+
+-- reverse order
+SELECT CategoryName, ProductName, UnitPrice, UnitsInStock
+FROM Categories C
+         JOIN Products P ON C.CategoryID = P.CategoryID
+ORDER BY CategoryName DESC, UnitPrice DESC;
+
+-- only one col reverse
+SELECT CategoryName, ProductName, UnitPrice, UnitsInStock
+FROM Categories C
+         JOIN Products P ON C.CategoryID = P.CategoryID
+ORDER BY CategoryName ASC, UnitPrice DESC;
+
+-- OFFSET FETCH
+-- 1st 5 rows
+SELECT CategoryName, ProductName, UnitPrice, UnitsInStock
+FROM Categories C
+         JOIN Products P ON C.CategoryID = P.CategoryID
+WHERE CategoryName = 'Confections'
+ORDER BY UnitPrice DESC
+OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY;
+
+-- The next 5
+SELECT CategoryName, ProductName, UnitPrice, UnitsInStock
+FROM Categories C
+         JOIN Products P ON C.CategoryID = P.CategoryID
+WHERE CategoryName = 'Confections'
+ORDER BY UnitPrice DESC
+OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY;
