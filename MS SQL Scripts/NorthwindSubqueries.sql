941b49ba2e4a44219320eaed4305562e06ccea55
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-24 13:25:00 -0500

    Adding Subquery and Views lesson scripts

---
+++ MS SQL Scripts/NorthwindSubqueries.sql
@@ -0,0 +1,37 @@
+﻿USE Northwind;
+GO
+
+-- list of orders for the 3 most expensive products
+SELECT DISTINCT (O.OrderID), O.OrderDate, C.CompanyName
+FROM Orders O
+         JOIN Customers C ON C.CustomerID = O.CustomerID
+         JOIN [Order Details] OD on O.OrderID = OD.OrderID
+WHERE OD.ProductID IN
+      (
+          -- subquery the most expensive products
+          SELECT TOP 3 ProductID
+          FROM Products P
+          ORDER BY P.UnitPrice DESC
+      )
+ORDER BY C.CompanyName, O.OrderID;
+
+-- rewrite of 1st query ^ using a join to a subquery
+SELECT DISTINCT (O.OrderID), O.OrderDate, C.CompanyName
+FROM Orders O
+         JOIN Customers C ON C.CustomerID = O.CustomerID
+         JOIN [Order Details] OD on O.OrderID = OD.OrderID
+         JOIN (SELECT TOP 3 P.ProductID
+               FROM Products P
+               ORDER BY P.UnitsInStock DESC) AS TopProducts
+              ON OD.ProductID = TopProducts.ProductID
+ORDER BY C.CompanyName, O.OrderID;
+
+-- max order on each order
+SELECT O.OrderID,
+       O.OrderDate,
+       (
+           SELECT MAX(OD.UnitPrice)
+           FROM [Order Details] OD
+           WHERE O.OrderDate = OD.OrderID
+       ) AS MaxUnitPrice
+FROM Orders O;
---
+++ MS SQL Scripts/NorthwindViewQueries.sql
@@ -0,0 +1,12 @@
+﻿USE Northwind;
+GO
+
+CREATE VIEW ProductsAndCategories AS
+SELECT P.ProductID, P.ProductName, C.CategoryName, S.CompanyName
+FROM Products P
+         JOIN Categories C ON C.CategoryID = P.CategoryID
+         JOIN Suppliers S ON P.SupplierID = S.SupplierID;
+GO
+
+SELECT *
+FROM ProductsAndCategories;
